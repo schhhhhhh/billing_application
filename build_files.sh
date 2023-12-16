7faff783436b3dda4fda9e0bf9ef08f60bd4d9d6
schhhhhhh <schzao@gmail.com>
2023-12-16 16:27:22 +0100

    fix error

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,12 @@
 # build_files.sh
 
 echo "Building the project..."
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make migration..."
-python3 manage.py makemigrations --no-input
-python3 manage.py migrate --no-input
+python3.9 manage.py makemigrations --no-input
+python3.9 manage.py migrate --no-input
 
 echo "Collect static..."
-python3 manage.py collectstatic --no-input --clear
+python3.9 manage.py collectstatic --no-input --clear
 
