3555c10bea72d14f53ca93f8c77035655e9c0f74
schhhhhhh <schzao@gmail.com>
2023-12-16 18:56:08 +0100

    fix error 005

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,14 @@
 # build_files.sh
 
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+#python3.9 -m pip install -r requirements.txt
 
-echo "Make migration..."
-python3.9 manage.py makemigrations --no-input
-python3.9 manage.py migrate --no-input
+#echo "Make migration..."
+#python3.9 manage.py makemigrations --no-input
+#python3.9 manage.py migrate --no-input
 
-echo "Collect static..."
-python3.9 manage.py collectstatic --no-input --clear
+#echo "Collect static..."
+#python3.9 manage.py collectstatic --no-input --clear
 
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
