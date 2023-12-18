955688d149220300413c8133eacb4dc648c29284
schhhhhhh <schzao@gmail.com>
2023-12-18 18:50:22 +0100

    fix error

--- billing_api/billing_api/settings.py
+++ billing_api/billing_api/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
