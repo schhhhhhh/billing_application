ddcb8d860058d20fd137d2ec1ae18b1af8fba06a
schhhhhhh <schzao@gmail.com>
2023-12-18 17:13:08 +0100

    fix debug 002

--- billing_api/billing_api/settings.py
+++ billing_api/billing_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,8 +158,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
-
+# STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
